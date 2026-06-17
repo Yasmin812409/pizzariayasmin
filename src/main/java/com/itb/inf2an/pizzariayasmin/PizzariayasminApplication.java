eac1a494d233fc2e1bfd31c41304c4e7f23259b9
Yasmin Silva Soares <rm98837@estudante.fieb.edu.br>
2026-06-16 22:18:21 -0300

    Criado ambiente de Dev

---
+++ src/main/java/com/itb/inf2an/pizzariayasmin/PizzariayasminApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.pizzariayasmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariayasminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariayasminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf2an/pizzariayasmin/PizzariayasminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2an.pizzariayasmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariayasminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
